a14560a5ce12d1e37b14b4bb6127a57f05d60d50
Rick <rick-ast@outlook.com>
2024-05-07 20:44:25 +0800

    add stream io for video and audio meetings

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,7 +1,13 @@
 import { FC, PropsWithChildren } from 'react'
 
+import StreamVideoProvider from '@/providers/StreamClientProvider'
+
 const Layout: FC<PropsWithChildren> = ({ children }) => {
-  return children
+  return (
+    <main>
+      <StreamVideoProvider>{children}</StreamVideoProvider>
+    </main>
+  )
 }
 
 export default Layout
